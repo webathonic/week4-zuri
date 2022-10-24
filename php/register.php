097e7837e2eccdb6a98057da35469bd3c4c5e63e
Oyetunde Adeola <114435497+webathonic@users.noreply.github.com>
2022-10-24 04:51:01 +0100

    Update register.php
    updated the code

--- php/register.php
+++ php/register.php
@@ -9,9 +9,20 @@ registerUser($username, $email, $password);
 }
 
 function registerUser($username, $email, $password){
+    $user= array[
+        'fullname' => $username,
+        'email'=> $email,
+        'password' => $password
+        
+        };
     //save data into the file
     $folder= "../storage/users.csv";
-    $detail = fopen($folder, "ra+");    
+    $detail = fopen($folder, "ra+"); 
+    while (!feof($detail)){
+        $line = fgetcsv($detail);
+        if ($line1==$email){
+            return true;
+    }
     fwrite($detail, "$username, $email, $password \n");
     
     fclose($detail);
